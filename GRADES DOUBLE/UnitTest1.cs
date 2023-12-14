220fb807d84bc2fa0eb933ef3f1bcf40d61b3b1e
user <user@DESKTOP-Q7SRQOE>
2023-12-14 19:42:31 +0200

    Add project files.

---
+++ GRADES DOUBLE/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GRADES_DOUBLE
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
